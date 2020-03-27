7bf4099eca632e5c8ddb668cc1ffcc84a9834c4b
A-Yuille <awyuille@gmail.com>
2020-03-26 23:44:04 -0400

    pop ups

--- app/src/main/java/com/example/destinybuildoptimizer/tab1.java
+++ app/src/main/java/com/example/destinybuildoptimizer/tab1.java
@@ -20,10 +20,9 @@ public class tab1 extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(LayoutInflater inflater, final ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_tab1, container, false);
     }
-
 }
