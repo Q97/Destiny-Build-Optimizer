069c3fa4ba840d8bb892ddef64c5c9f1fb069fd5
A-Yuille <awyuille@gmail.com>
2020-03-26 22:08:06 -0400

    button fix, settings screen

---
+++ app/src/main/java/com/example/destinybuildoptimizer/SettingsActivity.java
@@ -0,0 +1,13 @@
+package com.example.destinybuildoptimizer;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
