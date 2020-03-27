0da25055eed63e6588a00297686812f0e3a61f2e
A-Yuille <awyuille@gmail.com>
2020-03-27 11:40:26 -0400

    working demo swap

--- app/src/main/java/com/example/destinybuildoptimizer/tab1.java
+++ app/src/main/java/com/example/destinybuildoptimizer/tab1.java
@@ -2,6 +2,7 @@ package com.example.destinybuildoptimizer;
 
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,6 +12,9 @@ import android.widget.ImageButton;
 
 import androidx.fragment.app.Fragment;
 
+import static android.app.Activity.RESULT_CANCELED;
+import static android.app.Activity.RESULT_OK;
+
 
 /**
  * A simple {@link Fragment} subclass.
@@ -18,6 +22,7 @@ import androidx.fragment.app.Fragment;
 public class tab1 extends Fragment {
 
     public static final int REQUEST_CODE = 1;
+    ImageButton helmetbtn;
     public tab1() {
         // Required empty public constructor
     }
@@ -29,7 +34,7 @@ public class tab1 extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_tab1, container, false);
 
-        final ImageButton helmetbtn = (ImageButton) view.findViewById(R.id.helmet_tab1);
+        helmetbtn = (ImageButton) view.findViewById(R.id.helmet_tab1);
         helmetbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -75,6 +80,23 @@ public class tab1 extends Fragment {
         });
 
 
-        return view;
+
+            return view;
     }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+
+        if (requestCode == 1) {
+
+            if (resultCode == RESULT_OK) {
+                helmetbtn.setImageResource(R.drawable.iron_fellowship_casque);
+            }
+            if (resultCode == RESULT_CANCELED) {
+                //Do nothing?
+            }
+        }
+
+    }
+
+
 }
--- app/src/main/java/com/example/destinybuildoptimizer/tab4.java
+++ app/src/main/java/com/example/destinybuildoptimizer/tab4.java
@@ -1,6 +1,7 @@
 package com.example.destinybuildoptimizer;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,6 +9,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+
 
 
 /**
@@ -15,7 +18,7 @@ import android.view.ViewGroup;
  */
 public class tab4 extends Fragment {
 
-
+    public static final int REQUEST_CODE = 1;
     public tab4() {
         // Required empty public constructor
     }
@@ -24,8 +27,19 @@ public class tab4 extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tab4, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_tab4, container, false);
+
+        final ImageButton slot1 = (ImageButton) view.findViewById(R.id.vault_slot1);
+        slot1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), DetailActivity.class);
+                startActivityForResult(intent, REQUEST_CODE);
+            }
+        });
+
+        return view;
     }
 
 }
